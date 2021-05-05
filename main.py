6ea08f5c17212e9e9df527c2a4fe04bc0bd2d051
lowekoz <lowe.k172@yahoo.com>
2021-05-05 11:48:21 +0200

    feat:score system

--- main.py
+++ main.py
@@ -18,16 +18,20 @@ camera.setmethod(follow)
 
 pygame.init()
 
-screen = pygame.display.set_mode([WIDTH, HEIGHT+100])
+screen = pygame.display.set_mode([WIDTH, HEIGHT+200])
 game = pygame.Surface([WIDTH,HEIGHT])
 SpaceEngine = Engine(game,camera)
 particles = ParticleManager(SpaceEngine)
 particles.addPlayer(rocket)
 particles.generatePlanets()
 
+score = 0
+
 running = True
 while running:
+
     pressed_keys = pygame.key.get_pressed()
+    
 
     # Did the user click the window close button?
     for event in pygame.event.get():
@@ -46,10 +50,34 @@ while running:
     screen.blit(game,(0,0))
     game.fill((0, 0, 0))
 
+    # DRAW SCORE
+    white = (255, 255, 255)
+    green = (0, 255, 0)
+    blue = (0, 0, 128)
+    # create a font object.
+    # 1st parameter is the font file
+    # which is present in pygame.
+    # 2nd parameter is size of the font
+    font = pygame.font.Font('freesansbold.ttf', 32)
+    # create a text suface object,
+    # on which text is drawn on it.
+    scoreString = str(score)
+    text = font.render('SCORE: ' + scoreString, True, green, blue)
+    # create a rectangular object for the
+    # text surface object
+    textRect = text.get_rect()
+    textRect.bottomleft = (WIDTH//4 - 100, HEIGHT//4 + 650)
+    screen.blit(text, textRect)
+    #
+
     rocket.set_keys(pressed_keys)
     
     particles.update(dt)
 
+    if rocket.velocity.length() >= 10:
+        score += 0.001*rocket.velocity.length()
+    print(rocket.velocity.length(), "length")
+    print(score, "score")
     # particles.checkCollision() 
 
     camera.scroll()
